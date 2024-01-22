66a39477b1eb08431c5af08d836e4f8b5d4b80e1
Idris Adebowale <idrys01@gmail.com>
2024-01-22 08:48:10 +0000

    add README file and 0-rotate_2d_matrix module

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""This module define a function that rotates a
+given 2d matrix by 90 degree
+"""
+
+
+def rotate_2d_matrix(matrix):
+    """Rotate a matrix by 90 deg
+    """
+    n = len(matrix)
+    for i in range(n // 2):
+        for j in range(i, n - i - 1):
+            temp = matrix[i][j]
+            matrix[i][j] = matrix[n - j - 1][i]
+            matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1]
+            matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1]
+            matrix[j][n - i - 1] = temp
---
+++ 0x07-rotate_2d_matrix/README.md
@@ -0,0 +1,3 @@
+# Rotate 2D Matrix
+## Introduction
+This project define a python function that rotates a 2D matrix
